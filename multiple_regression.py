2e9c9f9d2b09aad803f2d9e8165240f014300b87
shayanalahyari <shayanforstock@gmail.com>
2024-07-06 23:17:12 +0330

    ...

--- multiple_regression.py
+++ multiple_regression.py
@@ -1,3 +1,12 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+
+### creating the dataset
+dataset = pd.read_csv('50_Startups.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+print(x)
+print("_______-")
+print(y)
