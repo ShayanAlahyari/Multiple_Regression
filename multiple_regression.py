cab17e9d250e14bbd0d920fba31b6ed34ed03db2
shayanalahyari <shayanforstock@gmail.com>
2024-07-07 17:45:54 +0330

    ...

--- multiple_regression.py
+++ multiple_regression.py
@@ -1,12 +1,17 @@
+# Importing the libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
-### creating the dataset
+# Creating the dataset
 dataset = pd.read_csv('50_Startups.csv')
 x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
+
+# Handling categorical predictors
+encoder = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[3])],remainder='passthrough')
+x = np.array(encoder.fit_transform(x))
 print(x)
-print("_______-")
-print(y)
